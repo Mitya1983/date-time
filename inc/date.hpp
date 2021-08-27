f6f0d23be8fc80bc32c7501bc489782e08f4ba8c
Mitya <tristan.it@outlook.com>
2021-08-27 21:24:23 +0300

    Additional documentation added

--- inc/date.hpp
+++ inc/date.hpp
@@ -6,67 +6,99 @@
 namespace tristan::date {
     
     using Days = std::chrono::duration<int64_t , std::ratio_divide<std::ratio<86400>, std::chrono::seconds::period>>;
-    using Weeks = std::chrono::duration<int64_t , std::ratio_divide<std::ratio<604800>, std::chrono::seconds::period>>;
-    using Years = std::chrono::duration<int64_t , std::ratio_divide<std::ratio<31556926>, std::chrono::seconds::period>>;
     
-    inline const auto non_leap_year_seconds = std::chrono::seconds(31536000);
-    inline const auto leap_year_seconds = std::chrono::seconds(31622400);
+    /// \brief Number of days in regular year
+    inline const uint16_t non_leap_year_days{365};
+    /// \brief Number of days in leap year
+    inline const uint16_t leap_year_days{366};
+    /// \brief Number of days from 1 January 1900 to 1 January 1970.
+    inline const uint16_t days_since_1900_to_1970{25567};
     inline const uint16_t start_year = 1900;
     
     class Date
     {
-        class Durations {
-            friend class Date;
-        public:
-            [[nodiscard]] auto yearsSince1900() const -> const Years& {return m_years_since_1900;}
-            [[nodiscard]] auto weeksFrom1900() const -> const Weeks& {return m_weeks_since_1900;}
-            [[nodiscard]] auto daysSince1900() const -> const Days& {return m_days_since_1900;}
-            [[nodiscard]] auto daysSinceYearStart() const -> const Days& {return m_days_since_year_start;}
-            [[nodiscard]] auto daysSinceMonthStart() const -> const Days& {return m_days_since_month_start;}
-        private:
-            Years m_years_since_1900;
-            Weeks m_weeks_since_1900;
-            Days m_days_since_1900;
-            Days m_days_since_year_start;
-            Days m_days_since_month_start;
-        };
+    
     public:
+        /// \brief Creates Date object which represent current system date.
         Date();
-        explicit Date(int day, int month, int year);
+        /// \brief Creates Date object with specified day, month and year.
+        /// \param day uint8_t.
+        /// \param month uint8_t.
+        /// \param year uint16_t.
+        /// \throws std::range_error.
+        explicit Date(uint8_t day, uint8_t month, uint16_t year);
+        /// \brief Creates Date object from std::string representing the date in [YYYYMMDD] or [YYYY-MM-DD] formats
+        /// \param iso_date - std::string
+        /// \throws std::invalid_argument - if time representation has invalid format
+        /// \throws std::range_error - if time representation has invalid values
         explicit Date(const std::string &iso_date);
+        /// \brief Copy constructor
         Date(const Date&) = default;
+        /// \brief Move constructor
         Date(Date&&) = default;
-        
+         
+         /// \brief Copy assignment operator
         auto operator=(const Date&) -> Date& = default;
+        /// \brief Move assignment operator
         auto operator=(Date&&) -> Date& = default;
+        /// \brief Operator equal
         auto operator==(const Date& r) const -> bool;
+        /// \brief Operator less
         auto operator<(const Date& r) const -> bool;
         
+        /// \brief Destructor
         virtual ~Date() = default;
-    
+        
+        /// \brief Adds days to Date object;
+        /// \param days uint64_t.
         void addDays(uint64_t days);
+        /// \brief Adds months to Date object;
+        /// \param days uint64_t.
         void addMonths(uint64_t months);
+        /// \brief Adds years to Date object;
+        /// \param days uint64_t.
         void addYears(uint64_t years);
-        void substractDays(uint64_t days);
-        void substractMonths(uint64_t months);
-        void substractYears(uint64_t years);
+        /// \brief Subtracts days to Date object;
+        /// \param days uint64_t.
+        void subtractDays(uint64_t days);
+        /// \brief Subtracts months to Date object;
+        /// \param days uint64_t.
+        void subtractMonths(uint64_t months);
+        /// \brief Subtracts years to Date object;
+        /// \param days uint64_t.
+        void subtractYears(uint64_t years);
         
-        auto dayOfTheMonth() const -> uint8_t;
-        auto dayOfTheWeek() const -> uint8_t;
-        auto month() const -> uint8_t;
-        auto year() const -> uint16_t;
-        
-        auto is_weekend() const -> bool;
-        [[nodiscard]] virtual auto toString() const -> std::string;
-        
-        static auto isLeapYear(uint16_t year) -> bool;
+        /// \brief Returns currently set day of the month.
+        /// \note This function returns actual, or otherworldly current, day of the months and not the total number of days passed in the month.
+        /// \return uint8_t.
+        [[nodiscard]] auto dayOfTheMonth() const -> uint8_t;
+        /// \brief Returns currently set day of the week.
+        /// \note This function returns actual, or otherworldly current, day of the week and not the total number of days passed in the week.
+        /// \return uint8_t.
+        [[nodiscard]] auto dayOfTheWeek() const -> uint8_t;
+        /// \brief Returns currently set month of the year.
+        /// \return uint8_t.
+        [[nodiscard]] auto month() const -> uint8_t;
+        /// \brief Returns currently set year.
+        /// \return uint8_t.
+        [[nodiscard]] auto year() const -> uint16_t;
     
+        /// \brief Returns if currently set day of the week is weekend.
+        /// \note Saturday and Sunday are considered as weekend days.
+        /// \return bool.
+        [[nodiscard]] auto isWeekend() const -> bool;
+        /// \brief Return string representation of date in YYYY-MM-DD format.
+        /// \return std::string.
+        [[nodiscard]] virtual auto toString() const -> std::string;
+        /// \brief Checks if year is leap year.
+        /// \param year uint16_t.
+        /// \return bool.
+        [[nodiscard]] static auto isLeapYear(uint16_t year) -> bool;
     
     protected:
     
     private:
-        Durations m_durations;
-        uint8_t m_month;
+        Days m_days_since_1900;
     };
     
     auto operator != (const Date &l, const Date &r) -> bool;
--- inc/time.hpp
+++ inc/time.hpp
@@ -8,6 +8,7 @@
 
 namespace tristan::time {
     
+    /// \brief Enum which represents precisions used in implementation.
     enum class Precision : uint8_t {
         MINUTES,
         SECONDS,
@@ -15,7 +16,7 @@ namespace tristan::time {
         MICROSECONDS,
         NANOSECONDS
     };
-    
+    /// \brief enum which represents possible time zone offsets.
     enum class TimeZone : int8_t {
         WEST_12 [[maybe_unused]],
         WEST_11 [[maybe_unused]],
@@ -51,160 +52,162 @@ class DayTime
     
 public:
     /// \brief Creates DayTime object from system clock. UTC time is expected.
-    /// \param precision Precision = Precision::SECONDS
+    /// \param precision Precision.
     explicit DayTime(Precision precision = Precision::SECONDS);
-    /// \brief Minutes precision constructor
-    /// \param hours uint8_t
-    /// \param minutes uint8_t
-    /// \throws std::range_error
+    /// \brief Minutes precision constructor.
+    /// \param hours uint8_t.
+    /// \param minutes uint8_t.
+    /// \throws std::range_error.
     explicit DayTime(uint8_t hours, uint8_t minutes);
-    /// \brief Seconds precision constructor
-    /// \param hours uint8_t
-    /// \param minutes uint8_t
-    /// \param seconds uint8_t
-    /// \throws std::range_error
+    /// \brief Seconds precision constructor.
+    /// \param hours uint8_t.
+    /// \param minutes uint8_t.
+    /// \param seconds uint8_t.
+    /// \throws std::range_error.
     explicit DayTime(uint8_t hours, uint8_t minutes, uint8_t seconds);
-    /// \brief Milliseconds precision constructor
-    /// \param hours uint8_t
-    /// \param minutes uint8_t
-    /// \param seconds uint8_t
-    /// \param milliseconds uint16_t
-    /// \throws std::range_error
+    /// \brief Milliseconds precision constructor.
+    /// \param hours uint8_t.
+    /// \param minutes uint8_t.
+    /// \param seconds uint8_t.
+    /// \param milliseconds uint16_t.
+    /// \throws std::range_error.
     explicit DayTime(uint8_t hours, uint8_t minutes, uint8_t seconds, uint16_t milliseconds);
-    /// \brief Microseconds precision constructor
-    /// \param hours uint8_t
-    /// \param minutes uint8_t
-    /// \param seconds uint8_t
-    /// \param milliseconds uint16_t
-    /// \param microseconds uint16_t
-    /// \throws std::range_error
+    /// \brief Microseconds precision constructor.
+    /// \param hours uint8_t.
+    /// \param minutes uint8_t.
+    /// \param seconds uint8_t.
+    /// \param milliseconds uint16_t.
+    /// \param microseconds uint16_t.
+    /// \throws std::range_error.
     explicit DayTime(uint8_t hours, uint8_t minutes, uint8_t seconds, uint16_t milliseconds, uint16_t microseconds);
-    /// \brief Nanoseconds precision constructor
-    /// \param hours uint8_t
-    /// \param minutes uint8_t
-    /// \param seconds uint8_t
-    /// \param milliseconds uint16_t
-    /// \param microseconds uint16_t
-    /// \param nanoseconds uint16_t
-    /// \throws std::range_error
+    /// \brief Nanoseconds precision constructor.
+    /// \param hours uint8_t.
+    /// \param minutes uint8_t.
+    /// \param seconds uint8_t.
+    /// \param milliseconds uint16_t.
+    /// \param microseconds uint16_t.
+    /// \param nanoseconds uint16_t.
+    /// \throws std::range_error.
     explicit DayTime(uint8_t hours, uint8_t minutes, uint8_t seconds, uint16_t milliseconds, uint16_t microseconds, uint16_t nanoseconds);
-    
-    /// \brief Parses the string provided and create time object
-    /// \param time std::string representing time in following formats
-    /// \formats[hours, minutes, seconds] - two digits
-    /// \formats[milliseconds, microseconds and nanoseconds] - three digits
-    /// \formats[hours:minutes] - Minutes precision
-    /// \formats[hours:minutes:seconds] - Seconds precision
-    /// \formats[hours:minutes:seconds.milliseconds] - Milliseconds precision
-    /// \formats[hours:minutes:seconds.milliseconds.microseconds] - Microseconds precision
-    /// \formats[hours:minutes:seconds.milliseconds.microseconds.nanoseconds] - Nanoseconds precision
-    /// \throws std::invali_argument, std::range_error
+    /// \brief Parses the string provided and create time object.
+    /// \param time std::string representing time in following formats.
+    /// \format[hours, minutes, seconds] - two digits.
+    /// \format[milliseconds, microseconds and nanoseconds] - three digits.
+    /// \format[hours:minutes] - Minutes precision.
+    /// \format[hours:minutes:seconds] - Seconds precision.
+    /// \format[hours:minutes:seconds.milliseconds] - Milliseconds precision.
+    /// \format[hours:minutes:seconds.milliseconds.microseconds] - Microseconds precision.
+    /// \format[hours:minutes:seconds.milliseconds.microseconds.nanoseconds] - Nanoseconds precision.
+    /// \throws std::invali_argument, std::range_error.
     explicit DayTime(const std::string &time);
-    
+    /// \brief Copy constructor
     DayTime(const DayTime&) = default;
+    /// \brief Move constructor
     DayTime(DayTime&&) = default;
-
+    /// \brief Copy assignment operator
     auto operator=(const DayTime&) -> DayTime& = default;
+    /// \brief Move assignment operator
     auto operator=(DayTime&&) -> DayTime& = default;
     /// \brief Checks if DayTimes objects are equal.
-    /// \note Precision is taken into account. That is if comparable objects are having different precision - false is returned
-    /// \param r const DayTime&
-    /// \return true if objects are equal and false otherwise
+    /// \note Precision is taken into account. That is if comparable objects are having different precision - false is returned.
+    /// \param r const DayTime&.
+    /// \return true if objects are equal and false otherwise.
     auto operator==(const DayTime& r) const -> bool;
-    /// \brief Checks if one DayTime object is less then other one
-    /// \note Precision is taken into account. That is if comparable objects are having different precision - false is returned
-    /// \param r const DayTime&
-    /// \return true if left object is less and false otherwise
+    /// \brief Checks if one DayTime object is less then other one.
+    /// \note Precision is taken into account. That is if comparable objects are having different precision - false is returned.
+    /// \param r const DayTime&.
+    /// \return true if left object is less and false otherwise.
     auto operator<(const DayTime& r) const -> bool;
-    
+    /// \brief Unary plus operator
     void operator += (const DayTime& r);
+    /// \brief Unary minus operator
     void operator -= (const DayTime& r);
-    
+    /// \brief Destructor
     virtual ~DayTime() = default;
     
-    /// \brief Sets timezone offset. Only ISO hour based offsets are considered
-    /// \param offset TimeZone
+    /// \brief Sets timezone offset. Only ISO hour based offsets are considered.
+    /// \param offset TimeZone.
     [[maybe_unused]] void setOffset(TimeZone offset) {m_offset = offset;}
     
-    /// \brief Adds hours to DayTime object. This is a convenience function which converts hours to minutes and invokes addMinutes(uint64_t minutes) function
-    /// \param hours uint64_t
+    /// \brief Adds hours to DayTime object. This is a convenience function which converts hours to minutes and invokes addMinutes(uint64_t minutes) function.
+    /// \param hours uint64_t.
     void addHours(uint64_t hours);
     /// \brief Adds minutes to DayTime object.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void addMinutes(uint64_t minutes);
     /// \brief Adds seconds to DayTime object.
     /// \note Precision is taken into account. That is in case of MINUTES precision if number of seconds is less then 1 minute operation is meaningless and will not have any effect.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void addSeconds(uint64_t seconds);
     /// \brief Adds milliseconds to DayTime object.
     /// \note Precision is taken into account. That is in case of SECONDS or MINUTES precision if number of milliseconds is less then 1 second operation is meaningless and will not have any effect.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void addMilliseconds(uint64_t milliseconds);
     /// \brief Adds microseconds to DayTime object.
     /// \note Precision is taken into account. That is in case of SECONDS, MINUTES or MILLISECONDS precision if number of microseconds is less then 1 millisecond operation is meaningless and will not have any effect.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void addMicroseconds(uint64_t microseconds);
     /// \brief Adds nanoseconds to DayTime object.
     /// \note Precision is taken into account. That is in case of SECONDS, MINUTES, MILLISECONDS or MICROSECONDS precision if number of nanosecond is less then 1 microsecond operation is meaningless and will not have any effect.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void addNanoseconds(uint64_t nanoseconds);
     /// \brief subtracts hours from DayTime object. This is a convenience function which converts hours to minutes and invokes subtractMinutes(uint64_t minutes) function
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void subtractHours(uint64_t hours);
     /// \brief subtracts minutes from DayTime object.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void subtractMinutes(uint64_t minutes);
     /// \brief subtracts seconds from DayTime object.
     /// \note Precision is taken into account. That is in case of MINUTES precision if number of seconds is less then 1 minute operation is meaningless and will not have any effect.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void subtractSeconds(uint64_t seconds);
     /// \brief subtracts milliseconds from DayTime object.
     /// \note Precision is taken into account. That is in case of SECONDS or MINUTES precision if number of milliseconds is less then 1 second operation is meaningless and will not have any effect.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void subtractMilliseconds(uint64_t milliseconds);
     /// \brief subtracts microseconds from DayTime object.
     /// \note Precision is taken into account. That is in case of SECONDS, MINUTES, MILLISECONDS or MICROSECONDS precision if number of nanosecond is less then 1 microsecond operation is meaningless and will not have any effect.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void subtractMicroseconds(uint64_t microseconds);
     /// \brief subtracts nanoseconds from DayTime object.
     /// \note Precision is taken into account. That is in case of SECONDS, MINUTES, MILLISECONDS or MICROSECONDS precision if number of nanosecond is less then 1 microsecond operation is meaningless and will not have any effect.
-    /// \param hours uint64_t
+    /// \param hours uint64_t.
     void subtractNanoseconds(uint64_t nanoseconds);
     
     /// \brief Returns number of hours passed since day start.
-    /// \return uint8_t
+    /// \return uint8_t.
     [[nodiscard]] auto hours() const  -> uint8_t;
     /// \brief Returns number of minutes passed since hour start.
-    /// \return uint8_t
+    /// \return uint8_t.
     [[nodiscard]] auto minutes() const -> uint8_t;
     /// \brief Returns number of seconds passed since minute start.
-    /// \return uint8_t
+    /// \return uint8_t.
     [[nodiscard]] auto seconds() const -> uint8_t;
     /// \brief Returns number of milliseconds passed since second start.
-    /// \return uint16_t
+    /// \return uint16_t.
     [[nodiscard]] auto milliseconds() const -> uint16_t;
     /// \brief Returns number of microseconds passed since millisecond start.
-    /// \return uint16_t
+    /// \return uint16_t.
     [[nodiscard]] auto microseconds() const -> uint16_t;
     /// \brief Returns number of nanoseconds passed since microsecond start.
-    /// \return uint16_t
+    /// \return uint16_t.
     [[nodiscard]] auto nanoseconds() const -> uint16_t;
     
     /// \brief Returns precision of DayTime object.
-    /// \return Precision
+    /// \return Precision.
     [[nodiscard]] auto precision() const -> Precision {return m_precision;}
     
-    /// \brief Creates DayTime object which represents localtime
-    /// \param precision Precision::SECONDS
-    /// \return DayTime
+    /// \brief Creates DayTime object which represents localtime.
+    /// \param precision Precision::SECONDS.
+    /// \return DayTime.
     [[nodiscard]] static auto localTime(Precision precision = Precision::SECONDS) -> DayTime;
-    /// \brief Generates string representation of time. By default return ISO standard representation in formats represented below. If show_precision is set to true each format will be suffixed by offset in form of +(-)hh
-    /// \return std::string to represent the time
-    /// \formats[hours:minutes] - Minutes precision
-    /// \formats[hours:minutes:seconds] - Seconds precision
-    /// \formats[hours:minutes:seconds.milliseconds] - Milliseconds precision
-    /// \formats[hours:minutes:seconds.milliseconds.microseconds] - Microseconds precision
-    /// \formats[hours:minutes:seconds.milliseconds.microseconds.nanoseconds] - Nanoseconds precision
+    /// \brief Generates string representation of time. By default return ISO standard representation in formats represented below. If show_precision is set to true each format will be suffixed by offset in form of +(-)hh.
+    /// \return std::string to represent the time.
+    /// \format[hours:minutes] - Minutes precision.
+    /// \format[hours:minutes:seconds] - Seconds precision.
+    /// \format[hours:minutes:seconds.milliseconds] - Milliseconds precision.
+    /// \format[hours:minutes:seconds.milliseconds.microseconds] - Microseconds precision.
+    /// \format[hours:minutes:seconds.milliseconds.microseconds.nanoseconds] - Nanoseconds precision.
     [[nodiscard]] virtual auto toString(bool show_offset) const -> std::string;
 
 protected:
@@ -227,16 +230,21 @@ private:
     void _subtractMicroseconds(uint64_t microseconds);
     void _subtractNanoseconds(uint64_t nanoseconds);
 };
-
+    /// \brief Operator not equal to
     auto operator != (const DayTime& l, const DayTime& r) -> bool;
+    /// \brief Operator greater than
     auto operator > (const DayTime& l, const DayTime& r) -> bool;
+    /// \brief Operator less than or equal to
     auto operator <= (const DayTime& l, const DayTime& r) -> bool;
+    /// \brief Operator greater than or equal to
     auto operator >= (const DayTime& l, const DayTime& r) -> bool;
     
+    /// \brief Addition operator
     auto operator + (const DayTime& l, const DayTime &r) -> DayTime;
+    /// \brief Subtraction operator
     auto operator - (const DayTime& l, const DayTime &r) -> DayTime;
-    
+    /// \brief std::ostream operator
     auto operator<<(std::ostream &out, const DayTime &time) -> std::ostream&;
-}//namespace Tristan
+}//namespace tristan::time
 
 #endif // TIME_HPP
