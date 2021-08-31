ca4c5a1dead905d3bdc91e60e1be7b4a94f6639c
Mitya <tristan.it@outlook.com>
2021-08-31 20:28:15 +0300

    Documentation added. Additional operators added.

--- inc/date_time.hpp
+++ inc/date_time.hpp
@@ -4,41 +4,66 @@
 #include "time.hpp"
 
 namespace tristan::date_time{
-
-class Date_Time
-{
-public:
-    Date_Time();
-    Date_Time(const Date_Time&) = default;
-    Date_Time(Date_Time&&) = default;
+/// \brief Class to store date and day time according
+/// \headerfile date_time.hpp
+    class Date_Time
+    {
+    public:
+        /// \brief Default constructor. Invokes default constructors for date and daytime objects.
+        Date_Time();
+        /// \brief Copy constructor
+        Date_Time(const Date_Time&) = default;
+        /// \brief Move constructor
+        Date_Time(Date_Time&&) = default;
+        
+        /// \brief Copy assignment operator
+        Date_Time& operator=(const Date_Time&) = default;
+        /// \brief Move assignment operator
+        Date_Time& operator=(Date_Time&&) = default;
+        
+        /// \brief Operator equal.
+        auto operator==(const Date_Time& other) const -> bool;
+        /// \brief Operator less then.
+        auto operator<(const Date_Time& other) const -> bool;
+        
+        ~Date_Time() = default;
+        /// \brief Setter with copy assignment
+        void setDate(const date::Date& date) {m_date = date;}
+        /// \brief Setter with move assignment
+        void setDate(date::Date&& date) {m_date = date;}
+        /// \brief Setter with copy assignment
+        void setTime(const time::DayTime& time) {m_day_time = time;}
+        /// \brief Setter with move assignment
+        void setTime(time::DayTime&& time) {m_day_time = time;}
+        
+        /// \brief Getter for date
+        /// \return const date::Date&
+        [[nodiscard]] auto date() const -> const date::Date& {return m_date;}
+        /// \brief Getter for time
+        /// \return const time::DayTime&
+        [[nodiscard]] auto time() const -> const time::DayTime& {return m_day_time;}
+        /// \brief Generates string representation of time. Returns [Date::toString][T][DayTime::toString]
+        /// \return std::string
+        [[nodiscard]] virtual auto toString() const -> std::string;
     
-    Date_Time& operator=(const Date_Time&) = default;
-    Date_Time& operator=(Date_Time&&) = default;
+    protected:
     
-    auto operator==(const Date_Time& other) -> bool;
-    auto operator<(const Date_Time& other) -> bool;
+    private:
+        date::Date m_date;
+        time::DayTime m_day_time;
+    };
     
-    ~Date_Time() = default;
+    /// \brief Operator not equal to.
+    auto operator != (const Date_Time &l, const Date_Time &r) -> bool;
+    /// \brief Operator greater than.
+    auto operator > (const Date_Time &l, const Date_Time &r) -> bool;
+    /// \brief Operator less than or equal to.
+    auto operator <= (const Date_Time &l, const Date_Time &r) -> bool;
+    /// \brief Operator greater than or equal to.
+    auto operator >= (const Date_Time &l, const Date_Time &r) -> bool;
+    /// \brief std::ostream operator.  Date_Time::toString() is used.
+    auto operator<<(std::ostream &out, const Date_Time &dt) -> std::ostream&;
     
-    void setDate(const date::Date& date) {m_date = date;}
-    void setDate(date::Date&& date) {m_date = date;}
-    void setTime(const time::DayTime& time) {m_day_time = time;}
-    void setTime(time::DayTime&& time) {m_day_time = time;}
-    
-    [[nodiscard]] auto date() const -> const date::Date& {return m_date;}
-    [[nodiscard]] auto time() const -> const time::DayTime& {return m_day_time;}
-    
-    [[nodiscard]] virtual auto toString() const -> std::string;
-
-protected:
-
-private:
-    date::Date m_date;
-    time::DayTime m_day_time;
-};
-
-auto operator<<(std::ostream &out, const Date_Time &dt) -> std::ostream&;
-
 }// namespace tristan::date_time
 
 #endif // DATE_TIME_HPP
--- src/date_time.cpp
+++ src/date_time.cpp
@@ -15,11 +15,11 @@ auto tristan::date_time::Date_Time::toString() const -> std::string{
     return dt;
 }
 
-auto tristan::date_time::Date_Time::operator==(const tristan::date_time::Date_Time& other) -> bool{
+auto tristan::date_time::Date_Time::operator==(const tristan::date_time::Date_Time& other) const -> bool{
     return m_date == other.m_date && m_day_time == other.m_day_time;
 }
 
-auto tristan::date_time::Date_Time::operator<(const tristan::date_time::Date_Time& other) -> bool{
+auto tristan::date_time::Date_Time::operator<(const tristan::date_time::Date_Time& other) const -> bool{
     if (m_date > other.m_date){
         return false;
     }
@@ -34,4 +34,24 @@ auto tristan::date_time::Date_Time::operator<(const tristan::date_time::Date_Tim
 auto tristan::date_time::operator<<(std::ostream &out, const tristan::date_time::Date_Time &dt) -> std::ostream&{
     out << dt.toString();
     return out;
+}
+
+auto
+tristan::date_time::operator!=(const tristan::date_time::Date_Time& l, const tristan::date_time::Date_Time& r) -> bool{
+    return !(l == r);
+}
+
+auto
+tristan::date_time::operator>(const tristan::date_time::Date_Time& l, const tristan::date_time::Date_Time& r) -> bool{
+    return !(l <= r);
+}
+
+auto
+tristan::date_time::operator<=(const tristan::date_time::Date_Time& l, const tristan::date_time::Date_Time& r) -> bool{
+    return (l < r || l == r);
+}
+
+auto
+tristan::date_time::operator>=(const tristan::date_time::Date_Time& l, const tristan::date_time::Date_Time& r) -> bool{
+    return (l > r || l == r);
 }
